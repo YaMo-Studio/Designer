73629abd8868a615a1fe52becf2304c3778ec24e
chenya <2227237581@qq.com>
2022-04-17 00:08:17 +0800

    Initial Commit

---
+++ loginpage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginpage
+//
+//  Created by 陈亚 on 2022/4/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
